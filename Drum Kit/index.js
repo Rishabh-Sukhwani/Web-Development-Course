5c1cbd6834ff1cecf86801ffe6f5c317f94e24c7
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-07-25 22:03:49 +0530

    Started Drum Kit Project

---
+++ Drum Kit/index.js
@@ -0,0 +1,9 @@
+
+
+for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        alert("I got clicked!");
+    });
+
+}
