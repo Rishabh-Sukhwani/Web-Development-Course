7209ea3ff59be2ac136f0d7318e314e43509c122
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-07-27 12:24:34 +0530

    Added eventListeners using jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,13 @@
-$("h1").text("Bye");
 
-$("button").text("<em>Hey</em>");
+$("button").click(function() {
+    $("h1").css("color", "purple");
+});
+
+$(document).keypress(function(event) {
+    console.log(event.key);
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "purple");
+});
