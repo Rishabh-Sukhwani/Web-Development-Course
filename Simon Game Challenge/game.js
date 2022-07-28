0f96aa6af71642ff0e4bb964c13b946f429ac7d2
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-07-28 21:40:16 +0530

    Added game over and minor changes

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -13,7 +13,7 @@ $(".btn").click(function() {
     playSound(userChosenColour);
     animatePress(userChosenColour);
 
-    checkAnswer(userClickedPattern-1);
+    checkAnswer(userClickedPattern.length-1);
 
 });
 
@@ -30,17 +30,13 @@ $(document).keypress(function event() {
 function nextSequence() {
 
     userClickedPattern = [];
-
     level++;
     $("#level-title").text("Level " + level);
-
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-  
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
     playSound(randomChosenColour);
 }
 
@@ -58,15 +54,27 @@ function animatePress(currentColour) {
 }
 
 function checkAnswer(currentLevel) {
-    if (userClickedPattern[currentLevel]===gamePattern[currentLevel]) {
-        console.log("success");
-        
-    }
-
-    if (userClickedPattern.length === gamePattern.length) {
-
-        setTimeout(function() {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length){
+          setTimeout(function () {
             nextSequence();
-        },1000);
-    }
+          }, 1000);
+        }
+      } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+  
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 200);
+  
+        startOver();
+      }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
