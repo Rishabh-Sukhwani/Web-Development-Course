79db3b729c2f148865c1ce060f97d57ffbf816dd
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-07-28 20:50:05 +0530

    Added more functionality

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -3,6 +3,8 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
@@ -11,20 +13,40 @@ $(".btn").click(function() {
     playSound(userChosenColour);
 });
 
+$(document).keypress(function event() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+
+});
+
 
 function nextSequence() {
 
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 
   
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  playSound(randomChosenColour);
+    playSound(randomChosenColour);
 }
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");},100
+    );
 }
