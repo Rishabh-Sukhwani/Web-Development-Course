ff044ce9ea98ab4f730af87878138dbfaa19cff4
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-07-29 18:38:45 +0530

    Started Node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+
+var mySuperHeroName = superheroes.random();
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperHeroName);
+console.log(mySuperVillainName);
