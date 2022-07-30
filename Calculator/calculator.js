9920b0739da20b1b3edb24089b7c9fb43a23bdcf
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-07-30 14:53:20 +0530

    Added bmi calculator challenge

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -9,19 +9,20 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
-
-    var num1 = Number(req.body.num1);
-    var num2 = Number(req.body.num2);
-
-    var result = num1 + num2;
-
-    res.send("The result of the calculation is " + result);
+app.get("/bmiCalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 
+app.post("/", function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
 
-app.listen(3000, function(){
+    var result = weight / (height*height);
+
+    res.send("The bmi is :" + result);
+});
+
+app.listen(3000, function(req, res){
     console.log("Server started at port 3000");
 });
-
