45a123a9eb67f0dc82e0d2b0aebae0ed680c197f
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-08-01 16:35:45 +0530

    Added CSS

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,23 +3,24 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     
-    var today = new Date();
+    let today = new Date();
     
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
 
 
@@ -33,7 +34,7 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem; 
+    let item = req.body.newItem; 
     items.push(item);
     res.redirect("/");
 });
