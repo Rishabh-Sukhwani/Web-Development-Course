676b5241828db9a8911d4789fca6ef3377282469
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-08-01 16:59:11 +0530

    Changes

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -26,17 +27,36 @@ app.get("/", function(req, res){
 
     
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItem: items
     });
 
 
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItem: workItems})
+    res.redirect("/work");
+});
+
+app.post("/work", function(req, res){
+   let item = req.body.newItem;
+   workItems.push(item);
+   res.sendFile("/work"); 
+   res.redirect("/work");
+});
+
 app.post("/", function(req, res){
-    let item = req.body.newItem; 
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
 });
 
 app.listen(3000, function() {
