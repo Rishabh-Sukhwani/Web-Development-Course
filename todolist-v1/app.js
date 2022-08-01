8149aec62cd3c6dd6f09b9f30bff70c1360be7f1
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2022-08-01 16:00:49 +0530

    Added basic structure for adding items to ToDo List

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,56 +3,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
+    var day = today.toLocaleDateString("en-US", options);
 
-        case 1:
-            day = "Monday";
-            break;
 
-        case 2:
-            day = "Tuesday";
-            break;
-
-        case 3:
-            day = "Wednesday";
-            break;
-
-        case 4:
-            day = "Thursday";
-            break;
-
-        case 5:
-            day = "Friday";
-            break;
-
-        case 6:
-            day = "Saturday";
-            break;
-
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }
 
     
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItem: items
     });
 
 
 });
 
-
+app.post("/", function(req, res){
+    var item = req.body.newItem; 
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
